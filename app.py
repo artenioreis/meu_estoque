40c46353f1f5f810707f581a1ba4be9079e97a3f
artenioreis <artenioreis@live.com>
2025-08-04 19:41:18 -0300

    jupy

--- app.py
+++ app.py
@@ -711,5 +711,9 @@ def backup():
 with app.app_context():
     db.create_all()
 
+#if __name__ == '__main__':
+    #app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
